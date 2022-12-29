4e5c65bda01fa7a251ca8cc4c1815ba039cb3005
vrung2323 <67440644+vrung2323@users.noreply.github.com>
2022-12-29 14:48:10 -0500

    tinkered with leadership section of site

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -75,13 +75,13 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/varun-gande.png"), 
-      label: "First slide label", 
+      img: require("../assets/img/cds_logo.png"), 
+      label: "Cornell Data Science", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
       img: require("../editable-stuff/varun-gande.png"), 
-      label: "Second slide label", 
+      label: "Undergraduate Teaching Assistant", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
   ],
